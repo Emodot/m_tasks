f674e95746e5a05bb9c2a45ed9a2ba0ccc8bd205
David Emaye <emayeodavid@gmail.com>
2023-04-01 12:37:41 +0100

    developing category cards

---
+++ lib/constant.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const iconsColor = Color.fromARGB(255, 89, 96, 143);
---
+++ lib/screens/home.dart
@@ -0,0 +1,167 @@
+// import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:m_tasks/constant.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        // title: const Text('mTasks'),
+        elevation: 0,
+        // toolbarHeight: 70,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25))),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            color: iconsColor,
+          ),
+          tooltip: 'Menu Icon',
+          onPressed: () {},
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              "assets/icons/MagnifyingGlass.svg",
+              width: 20,
+              colorFilter: const ColorFilter.mode(
+                iconsColor,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              "assets/icons/BellSimple.svg",
+              width: 20,
+              colorFilter: const ColorFilter.mode(
+                iconsColor,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "What's up, Joy!",
+                style: TextStyle(
+                  color: Color(0xFF28315B),
+                  fontWeight: FontWeight.w800,
+                  fontSize: 27,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                "CATEGORIES",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: iconsColor,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  CategoryCard(
+                    size: size,
+                    tasksNo: 34,
+                    categoryName: '',
+                    categoryColor: '',
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({
+    super.key,
+    required this.size,
+    required this.tasksNo,
+    required this.categoryName,
+    required this.categoryColor,
+  });
+
+  final Size size;
+  final int tasksNo;
+  final String categoryName;
+  final String categoryColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size.width * 0.5,
+      decoration: BoxDecoration(
+        // backgroundBlendMode: ,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: iconsColor.withOpacity(0.1),
+            offset: const Offset(
+              5.0,
+              5.0,
+            ),
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+          ),
+          const BoxShadow(
+            color: Colors.white,
+            offset: Offset(0.0, 0.0),
+            blurRadius: 0.0,
+            spreadRadius: 0.0,
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "$tasksNo Tasks",
+              style: const TextStyle(
+                color: iconsColor,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              categoryName,
+              style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+            ),
+            const Divider(
+              color: Colors.blue,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
